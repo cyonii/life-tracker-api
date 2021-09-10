b662c304adfda8101e581d2c78de250c251c7936
CY Kalu <cy23@tutanota.com>
2021-09-10 00:16:51 +0000

    Version API with namespacing

---
+++ app/controllers/api/v1/authentication_controller.rb
@@ -0,0 +1,17 @@
+module Api
+  module V1
+    class AuthenticationController < ApplicationController
+      skip_before_action :authenticate_request
+
+      def authenticate
+        command = AuthenticateUser.call(params[:email], params[:password])
+
+        if command.success?
+          render json: { auth_token: command.result }
+        else
+          render json: command.errors, status: :unauthorized
+        end
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :users, except: [:index]
-  post 'auth', to: 'authentication#authenticate'
+  namespace :api do
+    namespace :v1 do
+      resources :users, except: [:index]
+      post 'auth', to: 'authentication#authenticate'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
