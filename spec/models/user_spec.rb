394c1f86b5a18766eb09b8540bfc657afebc1678
CY Kalu <cy23@tutanota.com>
2021-09-10 09:49:30 +0000

    Test User resources

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:valid_attributes) { { username: 'jdoe', email: 'jdoe@email.com', password: 'pass123' } }
+
+  subject { described_class.new(valid_attributes) }
+
+  describe 'Validations' do
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a username' do
+      subject.username = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without an email' do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without a password' do
+      subject.password = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid with a duplicate username' do
+      subject.save
+      duplicate_user = described_class.new(valid_attributes)
+      expect(duplicate_user).to_not be_valid
+    end
+
+    it 'is not valid with a duplicate email' do
+      subject.save
+      duplicate_user = described_class.new(valid_attributes)
+      expect(duplicate_user).to_not be_valid
+    end
+
+    it 'is not valid with an invalid email' do
+      subject.email = 'jdoe@email'
+      expect(subject).to_not be_valid
+    end
+
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:email) }
+
+    it { should validate_length_of(:username).is_at_least(2) }
+    it { should validate_length_of(:password).is_at_least(6) }
+  end
 end
