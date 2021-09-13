bceb8cd059b211f058ff74aa03abfa6040ea0687
CY Kalu <cy23@tutanota.com>
2021-09-13 23:42:05 +0000

    Validate uniqueness of Activity name

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Activity, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name).case_insensitive }
   end
 end
