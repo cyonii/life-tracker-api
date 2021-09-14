6bc245e24c08a7bc4a3ce5b225761ad0cceb9371
CY Kalu <cy23@tutanota.com>
2021-09-14 10:04:03 +0000

    Add seed function to create default Activities

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+activities = %w[Sleep Exercise Work Study]
+
+activities.each do |activity|
+  Activity.create!(name: activity)
+end
