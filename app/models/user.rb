90568a6ad6fef7f389ed50d15a36a185e12f9901
CY Kalu <cy23@tutanota.com>
2021-09-14 13:26:45 +0000

    Format code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   validates :username, presence: true, length: { minimum: 2 }, uniqueness: { case_sensitive: false }
-  validates :email, presence: true,
-                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
-                    uniqueness: { case_sensitive: false }
+  validates :email,
+            presence: true,
+            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+            uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   has_secure_password
 end
