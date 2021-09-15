f5e5e87734312189ff20c58a79f49073673bf8f6
CY Kalu <cy23@tutanota.com>
2021-09-15 15:33:52 +0000

    Exclude unnecessary members in user hash

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -7,7 +7,11 @@ module API
         command = AuthenticateUser.call(params[:email], params[:password])
 
         if command.success?
-          user_hash = UserSerializer.new(command.user).as_json
+          user_hash = {
+            id: command.user.id,
+            username: command.user.username,
+            email: command.user.email
+          }
           render json: { auth_token: command.result, user: user_hash }, status: :ok
         else
           render json: command.errors, status: :unauthorized
