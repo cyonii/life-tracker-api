a105fb83cf6773a7ca2c9c94872c6853814597fb
CY Kalu <cy23@tutanota.com>
2021-09-26 08:18:13 +0000

    Allow an origin in cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['localhost:3001', 'https://lifetrackerapi.herokuapp.com']
+    origins ['localhost:3001', 'https://thelifetrackerapp.netlify.app']
 
     resource '*',
              headers: :any,
